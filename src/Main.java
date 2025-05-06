a532568d61171eef43d7559d8645c9acbcf049a9
hirokiasano1 <hirokiasanovieira@gmail.com>
2025-05-05 22:35:10 -0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa1 =  new Pessoa("Lucas",18);
+        Pessoa pessoa2 = new Pessoa("Gilberto",16);
+        System.out.println(pessoa1.getNome()+" "+pessoa1.maiorIdade());
+        System.out.println(pessoa2.getNome()+" "+pessoa2.maiorIdade());
+
+        if(pessoa1.getIdade()> pessoa2.getIdade())
+        {
+            System.out.println("A pessoa 1 é mais velha do que a pessoa 2");
+        } else if(pessoa1.getIdade()< pessoa2.getIdade()) {
+            System.out.println("A pessoa 2 é mais velha do que a pessoa 1");
+        }
+        else
+        {
+            System.out.println("Ambos tem a msm Idade");
+        }
+    }
+}
